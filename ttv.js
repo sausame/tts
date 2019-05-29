010ebada60e8601e71ae1463b3a886372eede646
sausame <bi_song@163.com>
2019-05-29 16:21:31 +0800

    Add text to video

--- ttv.js
+++ ttv.js
@@ -1,8 +1,54 @@
+function getInputValue(id) {
+
+  var element = document.getElementById(id);
+  if (null == element) {
+    return null;
+  }
+
+  return element.value;
+}
+
+function getSelectedRadioValue(name) {
+
+  var radios = document.getElementsByName(name);
+
+  for (var i = 0, length = radios.length; i < length; i++) {
+    if (radios[i].checked) {
+      return radios[i].value;
+    }
+  }
+}
+
+function displayImage(id, url) {
+
+  if (null == url || '' == url) {
+    return;
+  }
+
+  var element = document.getElementById(id);
+  if (null == element) {
+    return;
+  }
+
+  element.setAttribute('src', url);
+}
+
+function createImageHtml(contentIndex, imageIndex) {
+
+  var content = '<p>NO.' + (imageIndex + 1) + ' Image:';
+  content += '<img id="content_' + contentIndex + '_image_' + imageIndex + '" style="width:100%;"></img></p>';
+  content += '<input type="text" id="content_' + contentIndex + '_imageUrl_' + imageIndex + '"  placeholder="https://" value=""/>';
+  content += '</p>';
+
+  return content;
+}
+
 function addImage(contentIndex, imageCount) {
 
   var contentElement = document.getElementById('content_' + contentIndex);
   var imageElement = document.createElement('p');
-  imageElement.innerHTML = 'NO.' + (imageCount + 1) + ':<input type="text" id="content_' + contentIndex + '_imageUrl_' + imageCount + '" value=""/>';
+
+  imageElement.innerHTML = createImageHtml(contentIndex, imageCount);
   var button = document.getElementById('content_' + contentIndex + '_addImage');
 
   contentElement.insertBefore(imageElement, button);
@@ -16,12 +62,12 @@ function addContent(contentCount) {
   var element = document.createElement('div');
   element.setAttribute('id', 'content_' + contentCount);
 
-  var content = 'Content ' + (contentCount + 1) + ':</br>';
+  var content = '<h2>NO.' + (contentCount + 1) + ' Content</h2></hr>';
   content += '<p>Name:<input type="text" id="content_' + contentCount + '_name" value=""/></p>';
-  content += '<p>Image List:</p>';
-  content += '<p>NO.1:<input type="text" id="content_' + contentCount + '_imageUrl_0" value=""/></p>';
+  content += '<p>Text:<textarea id="content_' + contentCount + '_text" style="width:100%;" rows="5"></textarea></p>';
+  content += '<p><h3>Image List</h3></p>';
+  content += createImageHtml(contentCount, 0);
   content += '<button id="content_' + contentCount + '_addImage" onclick="addImage(' + contentCount + ', 1)">Add Image</button>';
-  content += '<p><textarea id="content_' + contentCount + '_text" style="width:100%;" rows="5"></textarea></p>';
 
   element.innerHTML = content;
 
@@ -30,4 +76,106 @@ function addContent(contentCount) {
   containerElement.insertBefore(element, button);
   button.setAttribute('onclick', 'addContent(' + (contentCount + 1) + ')');
 }
+
+function createRequest() {
+
+  var request = '{';
+
+  var coding = getSelectedRadioValue('coding');
+
+  request += '"coding": "' + coding + '",';
+  request += '"language": "' + getSelectedRadioValue('language') + '",';
+
+  var background = getInputValue('background');
+  displayImage('backgroundImage', background);
+
+  request += '"background": "' + background + '",';
+  request += '"width": "' + getInputValue('width') + '",';
+  request += '"height": "' + getInputValue('height') + '",';
+
+  var logo = getInputValue('logoUrl');
+  displayImage('logoImage', logo);
+
+  request += '"logo": "' + logo + '",';
+  request += '"logo-width": "' + getInputValue('logoWidth') + '",';
+  request += '"logo-height": "' + getInputValue('logoHeight') + '",';
+
+  var font = getInputValue('fontUrl');
+
+  request += '"font": "' + font + '",';
+
+  request += '"contents-list":[';
+
+  var count = 0;
+
+  for (var contentIndex = 0; true; contentIndex ++) {
+    var contentElement = document.getElementById('content_' + contentIndex);
+
+    if (null == contentElement) {
+      break;
+    }
+
+    var text = getInputValue('content_' + contentIndex + '_text');
+    if ('' == text) {
+      continue;
+    }
+
+    if (count > 0) {
+      request += ',';
+    }
+
+    request += '{';
+
+    if ('base64' == coding) {
+      text = encodeURIComponent(text);
+      text = window.btoa(text);
+    }
+
+    var name = getInputValue('content_' + contentIndex + '_name');
+    if ('' != name && 'base64' == coding) {
+      name = encodeURIComponent(name);
+      name = window.btoa(name);
+    }
+
+    request += '"name": "' + name + '",';
+    request += '"text": "' + text + '",';
+    request += '"image-urls-list":[';
+
+    var imageCount = 0;
+    for (var imageIndex = 0; true; imageIndex ++) {
+      var urlElement = document.getElementById('content_' + contentIndex + '_imageUrl_' + imageIndex);
+
+      if (null == urlElement) {
+        break;
+      }
+
+      var url = urlElement.value;
+      if ('' == url) {
+        continue;
+      }
+
+      if (imageCount > 0) {
+        request += ',';
+      }
+
+      request += '"' + url + '"';
+      displayImage('content_' + contentIndex + '_image_' + imageIndex, url);
+
+      imageCount ++;
+    }
+
+    request += ']';
+    request += '}';
+
+    count ++;
+  }
+
+  request += ']';
+  request += '}';
+
+  document.getElementById('request').value = request;
+  document.getElementById('msg').value = window.btoa(window.btoa(request));
+}
+
+addContent(0);
 
---
+++ ttv.php
@@ -0,0 +1,64 @@
+<?php
+
+function save($path, $data) {
+
+	if (!$fp = @fopen($path, 'wb')) {
+		return false;
+	}
+
+	fwrite($fp, $data);
+	@fclose($fp);
+
+	return true;
+}
+
+function runCommand($cmd, &$retval) {
+
+	syslog(LOG_WARNING, $cmd);
+	$retval = array();
+	exec($cmd, $retval);
+	$log = implode("\n", $retval);
+	syslog(LOG_WARNING, $log);
+	return $log
+}
+
+$msg = NULL;
+
+if (! empty($_POST)) {
+	$msg = $_POST['msg'];
+} elseif (! empty($_GET)) {
+	$msg = $_GET['msg'];
+}
+
+if (empty($msg)) {
+	die('No message.');
+}
+
+$config = parse_ini_file('config.ini');
+
+$codePath = $config['code-path'];
+$path = $config['save-path'];
+
+if (!is_dir($path)) {
+	mkdir($path, 0777);
+}
+
+$now = date('Y_m_d_H_i_s');
+
+$jsonPath = $path . '/'. $now . '.json';
+$savePath = $path . '/'. $now . '.mp4';
+
+$msg = base64_decode(base64_decode($msg));
+
+save($jsonPath, $msg);
+
+$cmd = "cd $codePath && source .env/bin/activive && ./blossoming.py config.ini templates/tts.json $jsonPath $savePath";
+
+echo("<p>$cmd</p>");
+//$log = runCommand($cmd, $retval);
+
+echo("<p><a href='files/$now.json'>$now.json</a></p>");
+echo("<p><a href='files/$now.mp4'>$now.mp4</a></p>");
+?>
+
+
